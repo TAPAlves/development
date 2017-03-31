7e86c98bf1ffc0e5dfb2abf62810ee25d27bfd50
tiagoalves <tiagoalves@talves-desktop>
2017-03-31 14:54:16 +0100

    [add] resources- ipc codes for patent classification - txt files

--- src/test/com/silicolife/textmining/patentpipeline/bulkData/getFilenamesTest.java
+++ src/test/com/silicolife/textmining/patentpipeline/bulkData/getFilenamesTest.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.junit.Test;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -21,18 +22,18 @@ public class getFilenamesTest {
 
 
 
-//		@Test
+		@Test
 	public void getFilenames() throws IOException, SAXException, ParserConfigurationException{
-		//		BDSSUtils.getFileNames("2016");
-		//		BDSSUtils.getBulkPatentFile("2017", (String) BDSSUtils.getFileNames("2017").toArray()[0]);
+//				BDSSUtils.getFileNames("2016");
+//				BDSSUtils.getBulkPatentFile("2017", (String) BDSSUtils.getFileNames("2017").toArray()[0]);
 		//		BDSSUtils.getBulkPatentFile("2017","ipg170103.zip");
 		//		BDSSUtils.getBulkPatentFile("2017","ipg170110.zip");
 		//System.out.println(BDSSUtils.getFileLenght("https://bulkdata.uspto.gov/data2/patent/grant/redbook/fulltext/2017/ipg170103.zip"));	
-		//		BDSSUtils.unzipPatentFullTextFile("tempZipFiles/ipg170103.zip", "tempZipFiles/ipg170103" );
-		Set<IPublication> pubs = BDSSUtils.parseXMLfile("tempZipFiles/ipg170103/ipg170103.xml");
-		for(IPublication pub:pubs){
-			System.out.println(pub);	
-		}
+				BDSSUtils.unzipPatentFullTextFile("tempZipFiles/ipg170117.zip", "tempZipFiles/ipg170117" );
+		Set<IPublication> pubs = BDSSUtils.parseXMLfile("tempZipFiles/ipg170117/ipg170117.xml");
+//		for(IPublication pub:pubs){
+//			System.out.println(pub);	
+//		}
 	}
 
 
