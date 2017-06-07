735733ba1b0ad7c87393eb4b317be7f6acf6731d
talves <tiago_alves26@hotmail.com>
2017-06-07 14:36:11 +0100

    Update project with source code

---
+++ src/devTest/main.java
@@ -0,0 +1,14 @@
+package devTest;
+
+import org.clapper.util.classutil.ClassFinder;
+import org.junit.Test;
+
+public class main {
+
+	
+	@Test
+	public void test1(){
+		ClassFinder finder = new ClassFinder();
+		
+	}
+}
